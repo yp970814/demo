c2e00c7a5b22c7f41dc604a42e4416f49e200af3
yp970814 <yuanping970814@163.com>
2024-03-01 16:11:23 +0800

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @Author yuanping970814@163.com
+ * @Date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
